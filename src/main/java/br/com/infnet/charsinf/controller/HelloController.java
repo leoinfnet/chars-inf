f177ca2540a221a5a24bbcf3130eb432b9ca58ce
leonardogloria <contato@leonardogloria.com.br>
2023-03-27 19:39:52 -0300

    Alteracao no controller

--- src/main/java/br/com/infnet/charsinf/controller/HelloController.java
+++ src/main/java/br/com/infnet/charsinf/controller/HelloController.java
@@ -12,6 +12,6 @@ import java.util.Map;
 public class HelloController {
     @GetMapping
     public ResponseEntity hello(){
-        return ResponseEntity.ok(Map.of("Message", "HELLO_FROM_JENKINS"));
+        return ResponseEntity.ok(Map.of("Message", "HELLO_FROM_JENKINS_2"));
     }
 }
